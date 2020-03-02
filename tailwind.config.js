82d54a114d6562110d56af5f34a7b687b5268f41
ricosmall <805163443@qq.com>
2020-03-03 01:39:14 +0800

    feat: initialize repository

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      print: { raw: 'print' }
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
